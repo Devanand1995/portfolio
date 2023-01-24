ce8b4b46a8fa9e2fd0c0fef24bd321f827269bae
devanand1995 <devaprof1@gmail.com>
2023-01-24 18:14:54 +0530

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './component/landing/landing.component';
+import { ContactComponent } from './component/contact/contact.component';
+import { EducationComponent } from './component/education/education.component';
+import { ExperienceComponent } from './component/experience/experience.component';
+import { VisionComponent } from './component/vision/vision.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { ProjectComponent } from './component/project/project.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    ContactComponent,
+    EducationComponent,
+    ExperienceComponent,
+    VisionComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProjectComponent
   ],
   imports: [
     BrowserModule,
